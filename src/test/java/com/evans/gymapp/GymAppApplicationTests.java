304b72de359373efda33f5c559c24e395e58c5e5
jonathan <jonathan.evans.1994@gmail.com>
2019-07-08 19:43:00 +0100

    Initial commit

---
+++ src/test/java/com/evans/gymapp/GymAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.evans.gymapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GymAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
