3f4a45b36b9c719acbbefb0b490c46bed3b5284c
jonathan <jonathan.evans.1994@gmail.com>
2019-07-22 19:38:19 +0100

    Added routine domain

---
+++ src/main/java/com/evans/gymapp/domain/Routine.java
@@ -0,0 +1,19 @@
+package com.evans.gymapp.domain;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.time.Instant;
+import java.util.List;
+
+@Data
+@Builder
+@RequiredArgsConstructor
+public class Routine {
+
+  private final String name;
+  private final List<Exercise> exercises;
+  private final Instant lastPerformedDateUtc;
+  private final String notes;
+}
