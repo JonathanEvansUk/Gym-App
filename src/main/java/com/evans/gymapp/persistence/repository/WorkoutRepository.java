0b78576ddbbbc8f48367e744c5b277c5be6ef646
jonathan <jonathan.evans.1994@gmail.com>
2019-07-22 19:34:53 +0100

    removed repo annotation

--- src/main/java/com/evans/gymapp/persistence/repository/WorkoutRepository.java
+++ src/main/java/com/evans/gymapp/persistence/repository/WorkoutRepository.java
@@ -2,7 +2,6 @@ package com.evans.gymapp.persistence.repository;
 
 import com.evans.gymapp.persistence.table.WorkoutEntity;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
