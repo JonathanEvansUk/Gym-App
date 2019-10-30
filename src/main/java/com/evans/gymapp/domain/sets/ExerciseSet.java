0c1a1478fece7160b531f6393b6f8e2bdebdf374
jonathan <jonathan.evans.1994@gmail.com>
2019-10-30 19:14:04 +0000

    fixed deserialization of exercise set

--- src/main/java/com/evans/gymapp/domain/sets/ExerciseSet.java
+++ src/main/java/com/evans/gymapp/domain/sets/ExerciseSet.java
@@ -1,10 +1,17 @@
 package com.evans.gymapp.domain.sets;
 
 import com.evans.gymapp.domain.Status;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.Data;
 
+import static com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+@JsonSubTypes({
+    @Type(value = WeightedSet.class),
+    @Type(value = NonWeightedSet.class)
+})
 @Data
 public abstract class ExerciseSet {
 
--- src/test/java/com/evans/gymapp/ExerciseSetDeserializationTest.java
+++ src/test/java/com/evans/gymapp/ExerciseSetDeserializationTest.java
@@ -3,7 +3,6 @@ package com.evans.gymapp;
 import com.evans.gymapp.domain.Status;
 import com.evans.gymapp.domain.sets.ExerciseSet;
 import com.evans.gymapp.domain.sets.WeightedSet;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -12,7 +11,10 @@ import org.springframework.boot.test.autoconfigure.json.JsonTest;
 
 import java.io.IOException;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @Slf4j
@@ -32,24 +34,17 @@ public class ExerciseSetDeserializationTest {
   }
 
   @Test
-  public void test() throws JsonProcessingException {
-
-    ExerciseSet exerciseSet = WeightedSet.builder()
-        .id(1L)
-        .weightKg(10D)
-        .numberOfReps(12)
-        .status(Status.COMPLETED)
-        .build();
-
-
-    log.info(objectMapper.writeValueAsString(exerciseSet));
-  }
-
-  @Test
   public void testDeserialize() throws IOException {
     String json = "{\"type\":\"WeightedSet\",\"id\":1,\"numberOfReps\":12,\"status\":\"COMPLETED\",\"weightKg\":10.0}";
     ExerciseSet exerciseSet = objectMapper.readValue(json, ExerciseSet.class);
 
     assertNotNull(exerciseSet);
+    assertThat(exerciseSet, instanceOf(WeightedSet.class));
+
+    WeightedSet weightedSet = (WeightedSet) exerciseSet;
+    assertEquals(1, weightedSet.getId());
+    assertEquals(12, weightedSet.getNumberOfReps());
+    assertEquals(Status.COMPLETED, weightedSet.getStatus());
+    assertEquals(10D, weightedSet.getWeightKg());
   }
 }
