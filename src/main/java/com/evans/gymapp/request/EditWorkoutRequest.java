7671cf51142db368f790f76df6c5d9e3adb326a6
jonathan <jonathan.evans.1994@gmail.com>
2020-03-24 19:44:33 +0000

    removed workoutName from editWorkoutRequest

--- src/main/java/com/evans/gymapp/request/EditWorkoutRequest.java
+++ src/main/java/com/evans/gymapp/request/EditWorkoutRequest.java
@@ -3,7 +3,6 @@ package com.evans.gymapp.request;
 import com.evans.gymapp.domain.WorkoutType;
 import lombok.*;
 
-import javax.validation.constraints.NotBlank;
 import java.time.Instant;
 
 @Getter
@@ -11,10 +10,6 @@ import java.time.Instant;
 @EqualsAndHashCode
 @RequiredArgsConstructor
 public class EditWorkoutRequest {
-
-  @NonNull
-  @NotBlank
-  private final String workoutName;
 
   @NonNull
   private final WorkoutType workoutType;
