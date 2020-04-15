8f1659b167ad015f861d48773f5d8bc1935ede28
jonathan <jonathan.evans.1994@gmail.com>
2020-04-15 16:41:48 +0100

    refactor exercise converter test

--- src/test/java/com/evans/gymapp/util/converter/ExerciseConverterTest.java
+++ src/test/java/com/evans/gymapp/util/converter/ExerciseConverterTest.java
@@ -1,47 +1,45 @@
 package com.evans.gymapp.util.converter;
 
 import com.evans.gymapp.domain.Exercise;
-import com.evans.gymapp.domain.MuscleGroup;
 import com.evans.gymapp.persistence.entity.ExerciseEntity;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.evans.gymapp.domain.MuscleGroup.BICEP;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ExerciseConverterTest {
 
   private final ExerciseConverter exerciseConverter = new ExerciseConverter();
 
+  private static final long ID = 1L;
+  private static final String NAME = "Bicep Curl";
+  private static final String INFORMATION = "Information";
+
+  private static final ExerciseEntity EXERCISE_ENTITY = ExerciseEntity.builder()
+      .id(ID)
+      .name(NAME)
+      .information(INFORMATION)
+      .muscleGroup(BICEP)
+      .build();
+
+  private static final Exercise EXERCISE = Exercise.builder()
+      .id(ID)
+      .name(NAME)
+      .information(INFORMATION)
+      .muscleGroup(BICEP)
+      .build();
+
   @Test
   public void convertFromEntity() {
-    ExerciseEntity exerciseEntity = ExerciseEntity.builder()
-        .id(1L)
-        .name("Bicep Curl")
-        .information("Bicep Curl with Dumbbell")
-        .muscleGroup(MuscleGroup.BICEP)
-        .build();
+    Exercise exercise = exerciseConverter.convert(EXERCISE_ENTITY);
 
-    Exercise exercise = exerciseConverter.convert(exerciseEntity);
-
-    assertEquals(exerciseEntity.getId(), exercise.getId());
-    assertEquals(exerciseEntity.getName(), exercise.getName());
-    assertEquals(exerciseEntity.getInformation(), exercise.getInformation());
-    assertEquals(exerciseEntity.getMuscleGroup(), exercise.getMuscleGroup());
+    assertEquals(EXERCISE, exercise);
   }
 
   @Test
   public void convertToEntity() {
-    Exercise exercise = Exercise.builder()
-        .id(1L)
-        .name("Bicep Curl")
-        .information("Bicep Curl with Dumbbell")
-        .muscleGroup(MuscleGroup.BICEP)
-        .build();
+    ExerciseEntity exerciseEntity = exerciseConverter.convert(EXERCISE);
 
-    ExerciseEntity exerciseEntity = exerciseConverter.convert(exercise);
-
-    assertEquals(exercise.getId(), exerciseEntity.getId());
-    assertEquals(exercise.getName(), exerciseEntity.getName());
-    assertEquals(exercise.getInformation(), exerciseEntity.getInformation());
-    assertEquals(exercise.getMuscleGroup(), exerciseEntity.getMuscleGroup());
+    assertEquals(EXERCISE_ENTITY, exerciseEntity);
   }
 }
